ef097b74d1ff56ca0ad3dcf696c2a8b42b36fd44
Karl Arthur Kade <karlarthur.kade@gmail.com>
2023-11-22 00:10:21 +0200

    Update SignupView.vue
    Sign up view passwords validation functionality done, also changed appearance

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,25 +3,31 @@
     <div class="box">
         <div>
             <h2>Welcome to PostIt</h2>
-            <a href="#">Create an account</a>
-            <p>or</p>
-            <p>Please log in</p>
-            <form for="login" action="index.html">
-                <div class="input">
-                    <label for="email">
-                        <input type="email" placeholder="Email" required>
-                    </label>
+            <p>Sign up</p>
+            <form class="form" for="login" v-on:submit="Login">
+                <div class="text_inputs">
+                    <div class="text">
+                        <b>Email:</b>
+                        <b>Password:</b>
+                    </div>
+                    <div class="inputs">
+                        <label for="email">
+                            <input type="email" placeholder="Email" required>
+                        </label>
+                        <label for="password">
+                            <input type="password" v-model="password" placeholder="Password" required>
+                        </label>
+                    </div>
                 </div>
                 <br>
-                <div class="input">
-                    <label for="password">
-                        <input type="password" placeholder="Password" required>
-                    </label>
-                </div>
-                <br>
-                <button type="submit" value="Log in">Log in</button>
+                <button type="submit" value="Log in">Sign up</button>
             </form>
-            <a href="#">Forget password</a>
+            <p class="errors" v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
         </div>
     </div>
 </body>
@@ -29,34 +35,66 @@
 
 <script>
 export default {
-  name: 'SignupView'
+  name: 'SignupView',
+  data: () => {
+    return {
+        errors: [],
+        password: '',
+    };
+  },
+  methods:{
+    Login: function() {
+        this.errors = [];
+        var correct = true;
+        if(this.password.length < 8 || this.password.length >= 15){
+            this.errors.push('Password should contain at least 8 characters and less than 15 characters.')
+            correct = false;
+        }
+        if(!/[A-Z]/.test(this.password)){
+            this.errors.push('Password should contain at least one uppercase character.')
+            correct = false;
+        }
+        if(!/[a-z].*[a-z]/.test(this.password)){
+            this.errors.push('Password should contain at least two lowercase characters.')
+            correct = false;
+        }
+        if(!/[0-9]/.test(this.password)){
+            this.errors.push('Password should contain at least one numeric character.')
+            correct = false;
+        }
+        if(!/[A-Z]/.test(this.password.charAt(0))){
+            this.errors.push('Password should start with uppercase character.')
+            correct = false;
+        }
+        if(!/_/.test(this.password)){
+            this.errors.push('Password should include "_".')
+            correct = false;
+        }
+        if(correct){
+            this.$router.push({ path: '/' });
+        }
+    },
+  }
 };
 </script>
 
 <style scoped>
 .box{
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 }
-.box > div{
-    width: 20em;
-    height: 24em;
-    background-color: rgb(224, 224, 224);
-    border-radius: 10px;
-    text-align: center;
-    margin: 10em auto;
-}
+
 a{
     color: rgb(89, 80, 255);
     text-decoration: none;
 }
-label input{
+.inputs > label > input{
     outline: none;
     border: none;
     padding: 0.5em 0em;
-    width: 32%;
-    margin: -0.2em;
+    margin: 0.6em;
 }
 
 input::placeholder{
@@ -85,20 +123,13 @@ a:hover{
 
 
 .box > div{
-      width: 20em;
+      width: 22em;
       height: 20em;
       background-color: rgb(224, 224, 224);
       border-radius: 10px;
       text-align: center;
-      margin: 10em auto;
-  }
- 
-  label input{
-      outline: none;
-      border: none;
-      padding: 0.5em 0em;
-      width: 32%;
-      margin: -0.2em;
+      margin: 8em auto;
+      margin-bottom: 0em;
   }
 
   input::placeholder{
@@ -152,5 +183,36 @@ a:hover{
       padding: 0.5em;
       margin: 0.3em;
       margin-bottom: 3em;
+  }
+
+  .errors{
+    display: flex;
+    flex-direction: column;
+    font-size: small;
+    padding-top: 1.5em;
+  }
+  .form{
+    padding-top: 2em;
+  }
+
+  .text_inputs{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text{
+    display: flex;
+    flex-direction: column;
+  }
+  .text > b{
+    text-align: right;
+    margin: 0.8em;
+  }
+
+  .inputs{
+    display: flex;
+    flex-direction: column;
   }
 </style>
