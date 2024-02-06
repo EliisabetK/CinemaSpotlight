ba4b41e64b5fec8d5d69d84c83b0b1b84e86a16d
Eliisabet <eliisabet.kaasik@gmail.com>
2024-02-06 17:52:09 +0200

    fixed rotten tomatoes scraper, letterboxd scraper

--- src/scripts/scraper.py
+++ src/scripts/scraper.py
@@ -5,6 +5,16 @@ from psycopg2 import sql
 from datetime import datetime
 import requests
 import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
+
 
 # Function to convert movie length like '2h15' to minutes
 def convert_runtime_to_minutes(runtime):
@@ -26,10 +36,8 @@ def trim_summary(summary):
 def process_image_link(cover):
     # Add "http://" to the beginning of the image link
     cover = "http://" + cover
-
     # Remove unnecessary parts from the image link
     cover = cover.replace('//images.markus.live/', '').split('?')[0]
-
     # Remove everything that comes after .jpg
     cover = cover.split('.jpg')[0] + '.jpg'
 
@@ -48,14 +56,83 @@ def get_movie_details_from_tmdb(title, api_key):
             return movie_details['results'][0]  # Return the first result
     return None
 
+
+# Function to get movie score from IMDb using Selenium
+def get_movie_score_from_rt(title):
+    # Setup Selenium
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--log-level=3')  # Suppress console logs
+    driver = webdriver.Chrome(service=service, options=options)
+
+    base_url = f'https://www.rottentomatoes.com/search?search={title}'
+
+    driver.get(base_url)
+    
+    try:
+        button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+        button.click()
+
+        # Wait for the movie page to load and find the score element
+        element = WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.TAG_NAME, 'search-page-media-row'))
+        )
+        score = element.get_attribute('tomatometerscore')
+        
+    except TimeoutException:
+        score = None
+        print("Accept button not found or not clickable within 1 second.")
+
+    driver.quit()
+    return score
+
+# Function to get movie score from Letterboxd using Selenium
+def get_movie_score_from_letterboxd(title):
+    # Setup Selenium
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--log-level=3')  # Suppress console logs
+    driver = webdriver.Chrome(service=service, options=options)
+
+    base_url = f'https://letterboxd.com/search/films/{title}'
+    driver.get(base_url)
+
+    try:
+        # Wait for the consent button to load and click it
+        consent_button = WebDriverWait(driver, 0.5).until(
+            EC.presence_of_element_located((By.XPATH, '//p[text()="Consent"]'))
+        )
+        consent_button.click()
+
+        # Wait for the page to load and find the first movie element
+        first_movie_element = WebDriverWait(driver, 0.5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.film-poster'))
+        )
+
+        # Wait until the overlay is no longer present
+        WebDriverWait(driver, 0.5).until_not(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.fc-dialog-overlay'))
+        )
+
+        first_movie_element.click()
+
+        # Wait for the movie page to load and find the score element
+        score_element = WebDriverWait(driver, 0.5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.average-rating'))
+        )
+        score = float(score_element.text.strip())
+    except TimeoutException:
+        score = None
+
+    driver.quit()
+    return score
+
+
 # Function to scrape movie data from the Apollo page
 def scrape_movie_data(url, api_key):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
     movies = []
-
-    # Replace these selectors with the actual HTML elements from the target website
     movie_elements = soup.select('.movie-card')
     for movie_element in movie_elements:
         score = None
@@ -95,19 +172,25 @@ def scrape_movie_data(url, api_key):
             releasedate = datetime.strptime(releasedate_str, '%Y-%m-%d') if releasedate_str else None
 
             score = tmdb_movie_details['vote_average']
-            movies.append({
-                'title': title,
-                'link': link,
-                'cover': cover,
-                'director': director,  # Add director to movie data
-                'runtime': runtime_in_minutes,
-                'releasedate': releasedate,  # Add releasedate to movie data
-                'summary': summary,  # Add summary to movie data
-                'score': score,  # Add the score to your movie data
-            })
-        print(f"Title: {title}, Director: {director}, Length: {runtime_in_minutes}, Summary: {summary}, Score: {score}")
-    return movies
 
+        # Get the Letterboxd score
+        letterboxd_score = get_movie_score_from_letterboxd(title)
+        rt_score = get_movie_score_from_rt(title)
+
+        movies.append({
+            'title': title,
+            'link': link,
+            'cover': cover,
+            'director': director,
+            'runtime': runtime_in_minutes,
+            'releasedate': releasedate,
+            'summary': summary,
+            'score': score,
+            'letterboxd_score': letterboxd_score, 
+            'rt_score': rt_score,
+                })
+        print(f"Title: {title}, Director: {director}, Length: {runtime_in_minutes}, Summary: {summary}, TMDB Score: {score}, Letterboxd Score: {letterboxd_score}, Rotten Tomatoes Score: {rt_score}")
+    return movies
 
 # Function to save data to PostgreSQL database
 def save_to_database(movies):
@@ -154,4 +237,3 @@ def save_to_database(movies):
 api_key = 'de150413238cb1ec0252f5c6374f3691'
 url = 'https://www.apollokino.ee/eng/movies?fromLang=1001'
 movies_data = scrape_movie_data(url, api_key)
-save_to_database(movies_data)
